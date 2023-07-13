20a1016732c0d01774b007872731bd144293f9fb
Bobby K Bose <57045659+bobby-bose@users.noreply.github.com>
2023-07-13 13:18:27 +0530

    Update settings.py

--- luminar/settings.py
+++ luminar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p&lela27*!5%tvdzije&o@f@j)!tq_t_@+0id+$(yeju^92#1v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
